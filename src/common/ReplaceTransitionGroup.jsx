630d3d673b86ab919c1fcae5ea97c1911de1b5c6
jquense <monastic.panic@gmail.com>
2014-11-05 16:59:54 -0500

    experiments

--- src/common/ReplaceTransitionGroup.jsx
+++ src/common/ReplaceTransitionGroup.jsx
@@ -13,6 +13,9 @@ var React = require('react')
   , $ = require('../util/dom')
   , _ = require('../util/_');
 
+
+
+
 module.exports = React.createClass({
 
   displayName: 'ReplaceTransitionGroup',
@@ -36,54 +39,50 @@ module.exports = React.createClass({
   },
 
   getInitialState: function() {
+    // var map = React.Children.map(this.props.children, c => c)
+    //   , current; 
+
+    // _.find(map, (c, key) => current = key, true)
+
     return {
-      children: React.Children.map(this.props.children, function(c){ return c })
+      last:     React.Children.only(this.props.children),
+      next:     null,
+      children: React.Children.map(this.props.children, c => c)
     };
   },
 
   componentWillReceiveProps: function(nextProps) {
-    var nextChildMapping = React.Children.map(nextProps.children, function(c){ return c })
-      , prevChildMapping = this.state.children
-      , key, hasOther, isNext;
+    var nextChild    = getChild(nextProps.children)
+      , prevChildren = this.state.children
+      , next = this.state.next
+      , last = this.state.last
+      , remove;
 
-    //console.log(prevChildMapping, nextChildMapping)
+    var isLastChild = last && key(last) === key(nextChild)
+      , isNextChild = next && key(next) === key(nextChild)
+      , isAnimating = this.animatingKeys[key(nextChild)];
+
+    if(!isLastChild && !isNextChild && !isAnimating){
+      this.last = next || last
+      this.next = nextChild
+    }
+
     this.setState({
-      children: _.merge(prevChildMapping, nextChildMapping)
+      last: this.last || last,
+      next: this.next || nextChild
     });
-
-    for(key in nextChildMapping) if( _.has(nextChildMapping, key) ) 
-    {
-      hasOther = prevChildMapping && _.has(prevChildMapping, key)
-      isNext   = !hasOther && !this.currentlyTransitioningKeys[key];
-
-      if ( isNext ) {
-        this.next = key
-        break
-      }
-    }
-
-    for(key in prevChildMapping) if( _.has(prevChildMapping, key) ) 
-    {
-      hasOther = nextChildMapping && _.has(nextChildMapping, key)
-      isNext   = !hasOther && !this.currentlyTransitioningKeys[key];
-
-      if ( isNext ) {
-        this.current = key
-        break
-      }
-    }
   },
 
   componentWillMount: function() {
-    this.currentlyTransitioningKeys = {};
+    this.animatingKeys = {};
     this.current = null;
     this.next    = null;
   },
 
   componentDidUpdate: function() {
-    var current = this.current
-      , next    = this.next
-      , first = this.refs[current || next] 
+    var last  = this.last
+      , next  = this.next
+      , first = this.refs.last || this.refs.next
       , node  = this.getDOMNode()
       , el    = first && first.getDOMNode()
       , ht, wt;
@@ -102,14 +101,14 @@ module.exports = React.createClass({
     this.props.onAnimating();
 
     this.next = null;
-    this.current = null;
+    this.last = null;
 
-    if ( next )   this.performEnter(next)
-    if ( current) this.performLeave(current)
+    if (next) this.performEnter('next')
+    if (last) this.performLeave('last')
   },
 
   performEnter: function(key) {
-    this.currentlyTransitioningKeys[key] = true;
+    this.animatingKeys[key] = true;
 
     var component = this.refs[key];
 
@@ -142,7 +141,7 @@ module.exports = React.createClass({
     if (component.componentDidEnter) 
       component.componentDidEnter();
     
-    delete this.currentlyTransitioningKeys[key];
+    delete this.animatingKeys[key];
 
     var currentChildMapping = React.Children.map(this.props.children, function(c){ return c })
 
@@ -153,13 +152,13 @@ module.exports = React.createClass({
   },
 
   isTransitioning: function(){
-    return Object.keys(this.currentlyTransitioningKeys).length !== 0
+    return Object.keys(this.animatingKeys).length !== 0
   },
 
   performLeave: function(key) {
     var component = this.refs[key];
 
-    this.currentlyTransitioningKeys[key] = true;
+    this.animatingKeys[key] = true;
 
     if (component.componentWillLeave) 
       component.componentWillLeave(this._handleDoneLeaving.bind(this, key));
@@ -174,7 +173,7 @@ module.exports = React.createClass({
     if (component.componentDidLeave) 
       component.componentDidLeave();
     
-    delete this.currentlyTransitioningKeys[key];
+    delete this.animatingKeys[key];
 
     var currentChildMapping = React.Children.map(this.props.children, function(c){ return c })
 
@@ -190,21 +189,36 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    var childrenToRender = {};
+    var childrenToRender = [
+          this.state.last && cloneWithProps(this.props.childFactory(this.state.last), { ref: 'last' }),
+          this.state.next && cloneWithProps(this.props.childFactory(this.state.next), { ref: 'next' }),
+        ];
 
-
-    for (var key in this.state.children) {
-
-      var child = this.state.children[key];
-
-      if (child) {
-        childrenToRender[key] = cloneWithProps(
-          this.props.childFactory(child),
-          { ref: key }
-        );
-      }
-    }
-
-    return transferPropsTo(this.props, this.props.component(null, childrenToRender));
+    return transferPropsTo(this.props, 
+            this.props.component(null, childrenToRender));
   }
 });
+
+function getChild(children){
+  // var map = React.Children.map(React.Children.only(this.props.children), c => c), key; 
+  // _.find(map, (c, k) => key = k)
+  return React.Children.only(children)
+}
+
+//CHANGE 0.12.0
+function key(child){
+  return child.props.key
+}
+
+
+function mergeChildren(children, current, next){
+  if(Object.keys(children).length > 2) throw new TypeError()
+
+  var newCurrent = _.find(children, (c, k) => k === current)
+    , newNext    = _.find(children, (c, k) => k !== newCurrent);
+
+  return {
+    current: newCurrent,
+    next:    newNext || next
+  }
+}
