c6923ba24b9cb9a36b652f34ec3d5a24bafad084
Jason Quense <monastic.panic@gmail.com>
2016-08-07 13:07:19 -0400

    fix

--- src/util/mountManager.js
+++ src/util/mountManager.js
@@ -3,8 +3,8 @@ import spyOnComponent from 'spy-on-component';
 export default function spyOnMount(componentInstance) {
   let mounted = true;
 
-  if (componentInstance.isMounted)
-    return componentInstance.isMounted.bind(componentInstance);
+  // if (componentInstance.isMounted)
+  //   return componentInstance.isMounted.bind(componentInstance);
 
   spyOnComponent(componentInstance, {
     componentWillUnmount() {
