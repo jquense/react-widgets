52e799960390cc9983f43e00e9d9386df46125ae
Jason Quense <monastic.panic@gmail.com>
2017-10-20 10:40:35 -0400

    fix virtualized list ref

--- packages/react-widgets/src/Listbox.js
+++ packages/react-widgets/src/Listbox.js
@@ -7,17 +7,22 @@ import { instanceId } from './util/widgetHelpers';
 const propTypes = {
   className: PropTypes.string,
   role: PropTypes.string,
+  nodeRef: PropTypes.func,
   emptyListMessage: PropTypes.node,
 };
 
 class Listbox extends React.Component {
   render() {
-    let { className, role, children, emptyListMessage, ...props } = this.props
+    let {
+      className, role, children, emptyListMessage, nodeRef, ...props
+    } = this.props
     let id = instanceId(this)
+
     return (
       <ul
         id={id}
         tabIndex='-1'
+        ref={nodeRef}
         className={cn(className, 'rw-list')}
         role={role === undefined ? 'listbox' : role }
         {...props}
