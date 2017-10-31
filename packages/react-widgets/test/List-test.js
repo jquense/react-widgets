68e1ec3754a46d9e9597658e55bc1846355a05ed
Jason Quense <monastic.panic@gmail.com>
2017-10-31 11:19:11 -0400

    lint

--- packages/react-widgets/test/List-test.js
+++ packages/react-widgets/test/List-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import List from '../src/List';
 
--- packages/react-widgets/test/NumberPicker-test.js
+++ packages/react-widgets/test/NumberPicker-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import NumberPicker from '../src/NumberPicker';
 
 let ControlledNumberPicker = NumberPicker.ControlledComponent;
