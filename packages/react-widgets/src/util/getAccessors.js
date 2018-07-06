6cd0929be0e76171249888ec5b77b7ddf9da5fac
Jimmy Jia <tesrin@gmail.com>
2018-07-06 14:56:47 -0400

    Make SelectList work with falsy values (#810)

--- packages/react-widgets/src/util/getAccessors.js
+++ packages/react-widgets/src/util/getAccessors.js
@@ -7,9 +7,6 @@ export default function createAccessors({ textField, valueField }) {
     indexOf: (data, item) => helpers.dataIndexOf(data, item, valueField),
     matches: (a, b) => helpers.valueMatcher(a, b, valueField),
     findOrSelf: (data, item) => helpers.dataItem(data, item, valueField),
-    find: (data, item) => {
-      let idx = helpers.dataIndexOf(data, item, valueField)
-      if (~idx) return data[idx]
-    },
+    includes: (data, item) => helpers.dataIndexOf(data, item, valueField) !== -1,
   }
 }
